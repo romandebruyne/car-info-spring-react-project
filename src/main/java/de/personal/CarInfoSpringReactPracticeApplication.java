e563dd538a9dc95534fcd18ecad59a8186f3a490
Hetterle, Roman (SE-A/35) <roman.hetterle@volkswagen.de>
2023-12-22 18:03:47 +0100

    init commit

---
+++ src/main/java/de/personal/CarInfoSpringReactPracticeApplication.java
@@ -0,0 +1,13 @@
+package de.personal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarInfoSpringReactPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarInfoSpringReactPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/personal/CarInfoSpringReactPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.personal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarInfoSpringReactPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
